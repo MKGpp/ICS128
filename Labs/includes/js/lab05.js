68e249824ffb8c14b1aa608cd194c528be836ff6
Matt Golshani <mattgolshani@Gmail.com>
2024-02-08 15:56:02 -0800

    fixed form validation regex and temporarily added form to main html page for testing

--- Labs/includes/js/lab05.js
+++ Labs/includes/js/lab05.js
@@ -2,42 +2,8 @@
 
 
 const loginBtn = () => {
-    const mainForm = document.getElementById("formDiv");
-    mainForm.innerHTML = `
-        <form class="w-50">
-                <div class="">
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1"><img src="./includes/images/person-circle.svg" alt="person"></span>
-                        <input type="text" class="form-control" placeholder="First Name" aria-label="firstName" aria-describedby="basic-addon1" id="firstName">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon2"><img src="./includes/images/person-circle.svg" alt="person"></span>
-                        <input type="text" class="form-control" placeholder="Last Name" aria-label="lastName" aria-describedby="basic-addon2" id="lastName">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon3"><img src="./includes/images/envelope-at-fill.svg" alt="email"></span>
-                        <input type="email" class="form-control" placeholder="Email Address" aria-label="email" aria-describedby="basic-addon3" id="emailAddy">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon4"><img src="./includes/images/123.svg" alt="age"></span>
-                        <input type="number" class="form-control" placeholder="Enter your Age" aria-label="age" aria-describedby="basic-addon4" id="ageInput">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon5"><img src="./includes/images/mailbox2-flag.svg" alt="postal code"></span>
-                        <input type="text" class="form-control" placeholder="Postal Code" aria-label="postalCode" aria-describedby="basic-addon5" id="psCode">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon6"><img src="./includes/images/telephone-forward-fill.svg" alt="phone number"></span>
-                        <input type="text" class="form-control" placeholder="Phone Number" aria-label="phoneNumber" aria-describedby="basic-addon6" id="phoneNum">
-                    </div>
-                    <div id="errorOutput" class="d-flex justify-content-center text-danger"></div>
-                    <div class="d-flex justify-content-end gap-2">
-                        <button type="submit" class="btn btn-outline-primary" id="formCancel" onclick="clearMain()">Cancel</button>
-                        <button type="submit" class="btn btn-outline-success" id="formSubmit" onclick="formSubmit()">Sign up</button>
-                    </div>
-                </div>
-        </form>
-    `
+    const signupForm = document.getElementById("formLogin");
+    signupForm.style.display = "block";
 }
 
 /***
@@ -50,9 +16,15 @@ const formSubmit = () => {
         let namePattern = /^[A-Za-z]+$/;
         let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         let phonePattern = /^(\d{3}[-\s]?){2}\d{4}$/;
-        let postalPattern = /^[a-zA-Z]\d[a-zA-Z]\d[a-zA-Z]\d$/;
-        let postalSpacePattern = /^[a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d$/
+        let postalPattern = /^[a-zA-Z]\d[a-zA-Z]\d[a-zA-Z]\d$|^[a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d$/;
 
+        document.getElementById("firstName").classList.remove('is-invalid');
+        document.getElementById("lastName").classList.remove('is-invalid');
+        document.getElementById("emailAddy").classList.remove('is-invalid');
+        document.getElementById("phoneNum").classList.remove('is-invalid');
+        document.getElementById("psCode").classList.remove('is-invalid');
+        document.getElementById("ageInput").classList.remove('is-invalid');
+        document.getElementById("errorOutput").innerHTML = '';
 
         const firstName = document.getElementById("firstName").value;
         const lastName = document.getElementById("lastName").value;
@@ -61,26 +33,33 @@ const formSubmit = () => {
         const postalCode = document.getElementById("psCode").value;
 
         if (!namePattern.test(firstName)) {
+            document.getElementById("firstName").classList.add('is-invalid');
             throw new Error("Name must be letters only! No spaces.");
         }
         if (!namePattern.test(lastName)) {
+            document.getElementById("lastName").classList.add('is-invalid');
             throw new Error("Name must be letters only! No spaces.");
         }
         if (document.getElementById("ageInput").value < 0 || document.getElementById("ageInput").value > 120) {
+            document.getElementById("ageInput").classList.add('is-invalid');
             throw new Error("Age must be between 0-120 inclusive!");
         }
         if (!emailPattern.test(emailInput)) {
+            document.getElementById("emailAddy").classList.add('is-invalid');
             throw new Error("Email Address Invalid! Format is \"sampleemail@email.com\"");
         }
         if (!phonePattern.test(phoneNumber)) {
+            document.getElementById("phoneNum").classList.add('is-invalid');
             throw new Error("Phone Number Invalid! Must be 000-000-0000 or 000 000 0000 or 0000000000!");
         }
-        if(!postalPattern.test(postalCode) || !postalSpacePattern.test(postalCode)) {
+        if(!postalPattern.test(postalCode)) {
+            document.getElementById("psCode").classList.add('is-invalid');
             throw new Error("Postal Code Invalid! Must be A1A 1A1 or A1A1A1!");
         }
 
     } catch (error) {
-        document.getElementById("errorOutput").innerHTML = `Error: ${error}`;
+        document.getElementById("errorOutput").innerHTML = `${error}`;
+
     }
 
 }
@@ -92,8 +71,8 @@ const formSubmit = () => {
  ***********************************************/
 
 const clearMain = () => {
-    const mainForm = document.getElementById("formDiv");
-    mainForm.innerHTML = '';
+    const signupForm = document.getElementById("formLogin");
+    signupForm.style.display = "none";
 }
 
 /***********************************************/
@@ -113,5 +92,7 @@ const loginLogout = () => {
  ***********************************/
 
 document.getElementById("logonBtn").addEventListener('click', loginBtn);
+document.getElementById("formSubmit").addEventListener('click', formSubmit);
+
 
 /***********************************/
