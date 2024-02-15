383241fea2b01b9727555e4b81e130354a6c3083
Matt Golshani <mattgolshani@Gmail.com>
2024-02-15 15:01:56 -0800

    i lied, now its actually working 100%

--- Labs/includes/js/lab05.js
+++ Labs/includes/js/lab05.js
@@ -5,11 +5,13 @@
 
 document.getElementById("formSubmit").addEventListener('click',() => {
     try {
+        //regex patterns for validating user information
         let namePattern = /^[A-Za-z]+$/;
         let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         let phonePattern = /^(\d{3}[-\s]?){2}\d{4}$/;
         let postalPattern = /^[a-zA-Z]\d[a-zA-Z]\d[a-zA-Z]\d$|^[a-zA-Z]\d[a-zA-Z] \d[a-zA-Z]\d$/;
 
+        //get the input values and store in constants
         const firstName = document.getElementById("firstName").value;
         const lastName = document.getElementById("lastName").value;
         const emailInput = document.getElementById("emailAddy").value;
@@ -26,47 +28,58 @@ document.getElementById("formSubmit").addEventListener('click',() => {
         document.getElementById("ageInput").classList.remove('is-invalid');
         document.getElementById("errorOutput").innerHTML = '';
 
+        //test inputs against regex patterns
         if (!namePattern.test(firstName)) {
             document.getElementById("firstName").classList.add('is-invalid');
             throw new Error("Name must be letters only! No spaces.");
         } else {
             document.getElementById("firstName").style.borderColor = "green";
+            document.getElementById("firstName").style.borderWidth = '3px';
         }
         if (!namePattern.test(lastName)) {
             document.getElementById("lastName").classList.add('is-invalid');
             throw new Error("Name must be letters only! No spaces.");
         } else {
             document.getElementById("lastName").style.borderColor = "green";
+            document.getElementById("lastName").style.borderWidth = '3px';
         }
         if (!emailPattern.test(emailInput)) {
             document.getElementById("emailAddy").classList.add('is-invalid');
             throw new Error("Email Address Invalid! Format is \"sampleemail@email.com\"");
         } else {
             document.getElementById("emailAddy").style.borderColor = "green";
+            document.getElementById("emailAddy").style.borderWidth = '3px';
         }
         if (document.getElementById("ageInput").value === "" || document.getElementById("ageInput").value < 0 || document.getElementById("ageInput").value > 120) {
             document.getElementById("ageInput").classList.add('is-invalid');
             throw new Error("Age must be between 0-120 inclusive!");
         } else {
             document.getElementById("ageInput").style.borderColor = "green";
+            document.getElementById("ageInput").style.borderWidth = '3px';
         }
         if (!postalPattern.test(postalCode)) {
             document.getElementById("psCode").classList.add('is-invalid');
             throw new Error("Postal Code Invalid! Must be A1A 1A1 or A1A1A1!");
         } else {
             document.getElementById("psCode").style.borderColor = "green";
+            document.getElementById("psCode").style.borderWidth = '3px';
         }
         if (!phonePattern.test(phoneNumber)) {
             document.getElementById("phoneNum").classList.add('is-invalid');
             throw new Error("Phone Number Invalid! Must be 000-000-0000 or 000 000 0000 or 0000000000!");
         } else {
             document.getElementById("phoneNum").style.borderColor = "green";
+            document.getElementById("phoneNum").style.borderWidth = '3px';
         }
+
+        //if all validated close the modal and populate/display user info card
         if (namePattern.test(firstName) && namePattern.test(lastName) && (ageInput >= 0 || ageInput <= 120 || ageInput !== "") && emailPattern.test(emailInput) && phonePattern.test(phoneNumber) && postalPattern.test(postalCode)) {
             document.getElementById('formSubmit').setAttribute('data-bs-dismiss', 'modal');
             document.getElementById("formSubmit").click();
             document.getElementById("greeting").innerHTML = `Hello ${firstName} ${lastName}`;
             formToCard(firstName, lastName, ageInput, emailInput, phoneNumber, postalCode);
+            
+            //swap login logout buttons
             login();
         }
 
@@ -84,6 +97,7 @@ document.getElementById("formSubmit").addEventListener('click',() => {
 const formToCard = (first, last, age, email, phone, postal) => {
     const userCard = document.getElementById("userCard");
 
+    //populate the card with users information and display it
     userCard.innerHTML = `
         <div class="card" style="width: 18rem;">
             <h1 class="d-flex justify-content-center">${first}'s Profile</h1>
@@ -131,12 +145,23 @@ const logout = () => {
  ***********************************/
 
 document.getElementById("logoutBtn").addEventListener('click', () => {
+
+    //Clear information fields and reset validated input color
     document.getElementById("userCard").innerHTML = "";
     document.getElementById("greeting").innerHTML = "";
     document.getElementById("formSubmit").removeAttribute('data-bs-dismiss');
+    document.getElementById("firstName").style.borderColor = null;
+    document.getElementById("lastName").style.borderColor = null;
+    document.getElementById("emailAddy").style.borderColor = null;
+    document.getElementById("ageInput").style.borderColor = null;
+    document.getElementById("psCode").style.borderColor = null;
+    document.getElementById("phoneNum").style.borderColor = null;
+
+    //swap the login and logout buttons
     logout();
 });
 document.getElementById("formCancel").addEventListener('click',() => {
+
     //Clear the form inputs when cancel is pressed
     let formClear = document.getElementById("formLogin").querySelectorAll('input');
     formClear.forEach(function(input){
