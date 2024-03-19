33d1f3ea847706051317d57bef2b09e2e63ba202
MKG <mattgolshani@gmail.com>
2024-03-19 13:12:49 -0700

    in class map stuff

--- script.js
+++ script.js
@@ -123,4 +123,128 @@
 //     xhttp.send();
 // };
 // loadDoc();
+let lat = 48.427236;
+let long = -123.366330;
+
+let uvicLat = 48.462414;
+let uvicLong = -123.311634;
+
+let christmasLat = 48.473984;
+let christmasLong = -123.376512;
+
+let mtDougLat = 48.492816;
+let mtDougLong = -123.346693;
+
+let thetisLat = 48.486960;
+let thetisLong = -123.458208;
+
+let uptownLat = 48.454045;
+let uptownLong = -123.375136;
+
+
+
+let myIcon = L.icon({
+    iconUrl: 'person.svg',
+    iconSize: [15, 15],
+    iconAnchor: [10, 10],
+    clickable: true,
+    title: "My Location",
+    zIndexOffset: 1000,
+    riseOnHover: true,
+    riseOffset: 500,
+});
+
+let map = L.map('map',{zoomControl: false}).setView([lat, long], 12);
+
+L.control.scale().addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let circle = L.circle([lat, long], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle.bindPopup("Victoria, BC");
+
+let circle2 = L.circle([uvicLat, uvicLong], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle2.bindPopup("Uvic Campus");
+
+let circle3 = L.circle([christmasLat, christmasLong], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle3.bindPopup("Christmas Hill");
+
+let circle4 = L.circle([mtDougLat, mtDougLong], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle4.bindPopup("Mt. Doug");
+
+let circle5 = L.circle([thetisLat, thetisLong], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle5.bindPopup("Thetis Lake");
+
+let circle6 = L.circle([uptownLat, uptownLong], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 150
+}).addTo(map);
+circle6.bindPopup("Uptown");
+
+
+let polygon = L.polygon([
+    [lat, long],
+    [uvicLat, uvicLong],
+    [christmasLat, christmasLong],
+    [mtDougLat, mtDougLong],
+    [thetisLat, thetisLong],
+    [uptownLat, uptownLong]
+]).addTo(map);
+
+let popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
+let findUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        let myLat = position.coords.latitude;
+        let myLong = position.coords.longitude;
+        localStorage.setItem("myLatitude", myLat);
+        localStorage.setItem("myLongitude", myLong);
+
+        const userLocation = L.marker(
+            [myLat, myLong],
+            {icon: myIcon}).addTo(map);
+
+        userLocation.setLatLng([myLat, myLong]);
+    });
+}
+findUserLocation();
+document.getElementById("btn").addEventListener('click', () => {
+    window.location ='second.html';
+});
 
